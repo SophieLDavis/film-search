de768909d95a91401b4d18e0481f2a08c49cf352
Seamus Barnett <seamus0689@gmail.com>
2023-02-01 18:33:56 +0000

    HTML setup

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+// Seamus test youtube api call
+// $("#search").on("click", function (e) {
+//     e.preventDefault();
+//     var queryURL,
+//       /** Get search input query */
+//       searchQuery = "harry potter soundtrack";
+//     // TODO: Get an API key
+//     queryURL =
+//       "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&key=AIzaSyDwgSkDF2C9Urcz4cS9A7r1XYsV5_khoh4&q=" +
+//       searchQuery;
+
+//     $.ajax({
+//       url: queryURL,
+//       method: "GET",
+//     }).then(function (result) {
+//       console.log(result);
+//     });
+//   });
